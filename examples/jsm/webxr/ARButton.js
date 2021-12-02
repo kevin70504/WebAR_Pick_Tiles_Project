14b293feb832aee9a4de8971673d0dcccd2d8f81
KevinChou <q502w07@gmail.com>
2021-12-02 16:44:32 +0800

    updata

--- examples/jsm/webxr/ARButton.js
+++ examples/jsm/webxr/ARButton.js
@@ -117,8 +117,8 @@ class ARButton {
 			button.style.display = '';
 
 			button.style.cursor = 'auto';
-			//button.style.left = 'calc(50% - 75px)';
-			button.style.right = '20px';
+			button.style.left = 'calc(50% - 75px)';
+			//button.style.right = '20px';
 			button.style.width = '150px';
 
 			button.onmouseenter = null;

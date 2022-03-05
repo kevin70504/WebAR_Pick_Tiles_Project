0a3b7d4e93034d17cfaaa919d530a0a3b36cd38c
KevinChou <q502w07@gmail.com>
2022-03-05 10:07:02 +0800

    updata

---
+++ examples/webxr_ar_hittest_Navigation.js
@@ -0,0 +1,10 @@
+
+export function openNav()
+{
+    document.getElementById("mySidenav").style.width = "180px";
+}
+		  
+export function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
